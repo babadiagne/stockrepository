6d97773d6a3df1a010ac4c653839d5ac91579a66
* <hamathdiagne1968@hotmail.com>
2022-06-08 20:45:29 +0000

    next commit

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Class1 (models.Model):
   auteurs = models.CharField (max_length = 42)
-  categorie = models.ForeignKey ('Class2' , on_delete=models.CASCADE)
+  larelation = models.ForeignKey ('Class2' , on_delete=models.CASCADE)
 #first class
   def __str__ (self):
     return self.auteurs
