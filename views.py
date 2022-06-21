fef1ef21903cf69c724c0511951691e6b766e528
* <hamathdiagne1968@hotmail.com>
2022-06-21 04:04:15 +0000

    first commit views

---
+++ views.py
@@ -0,0 +1,13 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def resume_curuculum (request):
+  return HttpResponse ("""
+<form method="post">
+<input type="text">
+<input type="email" placeholder="email">
+<input type="submit">
+</form>
+ """)
+
