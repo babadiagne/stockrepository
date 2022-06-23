eee8aff017e686525b1cc21475c7266b73f91f25
babadiagne <hamathdiagne1968@hotmail.com>
2022-06-23 19:26:43 +0000

    first description admin

--- admin.py
+++ admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from curuculum.models import Article,Categorie
+from curuculum.models import Article,Categorie,Client
 
-
-# Register your models here.
+# Register your models here : Article, Categorie, Client
 admin.site.register (Article)
 admin.site.register (Categorie)
+admin.site.register (Client)
+
