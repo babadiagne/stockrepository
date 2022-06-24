52b198efd5cd6da267339b8321fc00c056a7422a
babadiagne <pierrediagne1934@gmail.com>
2022-06-24 06:06:16 +0000

    commit friday 6:06

--- models.py
+++ models.py
@@ -7,12 +7,21 @@ class Article(models.Model):
   contenu=models.TextField(null=True)
   date=models.DateTimeField(auto_now_add=True,auto_now=False,verbose_name="Date de parution")
   categorie=models.ForeignKey('Categorie',on_delete=models.CASCADE)
+  client=models.ForeignKey('Client',on_delete=models.CASCADE)
+
 
   def __str__(self):
     return self.titre
 
 class Categorie(models.Model):
   nom=models.CharField(max_length=30)
+
   def __str__(self):
     return self.nom
+
+class Client(models.Model):
+  pnom=models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.pnom
 
